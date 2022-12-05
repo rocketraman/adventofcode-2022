00eb32bbfe4a0b5ec0a67ca1c113aa07faa3fb4a
Raman Gupta <rocketraman@gmail.com>
2022-12-04 23:26:36 -0500

    Day 4

--- calendar/day04/Day4.kt
+++ calendar/day04/Day4.kt
@@ -5,10 +5,28 @@ import Lines
 
 class Day4 : Day() {
   override fun part1(input: Lines): Any {
-    TODO("Not yet solved")
+    return input
+      .count { line ->
+        val (range1, range2) = line.split(",").map { p ->
+          p.split("-").let { it[0].toInt()..it[1].toInt() }
+        }
+        range1.containsRange(range2) || range2.containsRange(range1)
+      }
   }
 
   override fun part2(input: Lines): Any {
-    TODO("Not yet solved")
+    return input
+      .count { line ->
+        val (range1, range2) = line.split(",").map { p ->
+          p.split("-").let { it[0].toInt()..it[1].toInt() }
+        }
+        range1.overlapsRange(range2)
+      }
   }
+
+  fun IntRange.containsRange(other: IntRange): Boolean =
+    this.first <= other.first && this.last >= other.last
+
+  fun IntRange.overlapsRange(other: IntRange): Boolean =
+    this.first in other || this.last in other || other.first in this || other.last in this
 }
