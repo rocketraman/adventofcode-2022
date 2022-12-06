7155f59ab8fd601028a364d636e03d4d6052076d
Raman Gupta <rocketraman@gmail.com>
2022-12-06 00:13:03 -0500

    Day 6

--- calendar/day06/Day6.kt
+++ calendar/day06/Day6.kt
@@ -5,10 +5,23 @@ import Lines
 
 class Day6 : Day() {
   override fun part1(input: Lines): Any {
-    TODO("Not yet solved")
+    return findMarker(input, 4)
   }
 
   override fun part2(input: Lines): Any {
-    TODO("Not yet solved")
+    return findMarker(input, 14)
+  }
+
+  private fun findMarker(input: Lines, size: Int): Int {
+    val windows = input.single()
+      .windowed(size, partialWindows = true)
+      .withIndex()
+
+    for ((i, w) in windows) {
+      if (w.toSet().size == size) {
+        return i + size
+      }
+    }
+    error("No window found")
   }
 }
