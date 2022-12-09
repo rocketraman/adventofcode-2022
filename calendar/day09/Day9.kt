bbb37e9d3af778e872e3243412ed49891060cca4
Raman Gupta <rocketraman@gmail.com>
2022-12-09 01:45:11 -0500

    Day 9 cleanup

--- calendar/day09/Day9.kt
+++ calendar/day09/Day9.kt
@@ -5,56 +5,37 @@ import Lines
 
 class Day9 : Day() {
   data class Position(val x: Int, val y: Int) {
-    val adjacentPositions
-      get() = setOf(
-        Position(x - 1, y - 1),
-        Position(x - 1, y),
-        Position(x - 1, y + 1),
-        Position(x, y - 1),
-        Position(x, y + 1),
-        Position(x + 1, y - 1),
-        Position(x + 1, y),
-        Position(x + 1, y + 1),
-      )
-    val right
-      get() = copy(x = x + 1, y = y)
-    val up
-      get() = copy(x = x, y = y + 1)
-    val left
-      get() = copy(x = x - 1, y = y)
-    val down
-      get() = copy(x = x, y = y - 1)
+    val adjacentPositions get() = setOf(
+      Position(x, y),
+      Position(x - 1, y - 1),
+      Position(x - 1, y),
+      Position(x - 1, y + 1),
+      Position(x, y - 1),
+      Position(x, y + 1),
+      Position(x + 1, y - 1),
+      Position(x + 1, y),
+      Position(x + 1, y + 1),
+    )
+    val right get() = copy(x = x + 1, y = y)
+    val up get() = copy(x = x, y = y + 1)
+    val left get() = copy(x = x - 1, y = y)
+    val down get() = copy(x = x, y = y - 1)
   }
 
-  override fun part1(input: Lines): Any {
-    var head = Position(0, 0)
-    var tail = Position(0, 0)
-    val tailVisited = mutableSetOf(tail)
+  override fun part1(input: Lines): Any = simulate(input, 2)
 
-    inputToMoves(input)
-      .forEach { direction ->
-        head = head.move(direction)
-        tail = tail.follow(head)
-        tailVisited.add(tail)
-      }
+  override fun part2(input: Lines): Any = simulate(input, 10)
 
-    return tailVisited.size
-  }
-
-  override fun part2(input: Lines): Any {
-    val knots = MutableList(10) { Position(0, 0) }
-    val tailVisited = mutableSetOf(knots.last())
-
-    inputToMoves(input)
-      .forEach { direction ->
+  private fun simulate(input: Lines, knotCount: Int): Int {
+    val knots = MutableList(knotCount) { Position(0, 0) }
+    return buildSet {
+      add(knots.last())
+      inputToMoves(input).forEach { direction ->
         knots[0] = knots.first().move(direction)
-        for (k in 1..9) {
-          knots[k] = knots[k].follow(knots[k - 1])
-        }
-        tailVisited.add(knots.last())
+        for (k in 1 until knotCount) knots[k] = knots[k].follow(knots[k - 1])
+        add(knots.last())
       }
-
-    return tailVisited.size
+    }.size
   }
 
   private fun inputToMoves(input: Lines) = input
@@ -72,16 +53,10 @@ class Day9 : Day() {
     else -> error("Invalid direction $direction")
   }
 
-  private fun Position.follow(leader: Position): Position {
-    return if (this !in leader.adjacentPositions) {
-      // move this to follow head and add the new position to the visited list
-      when {
-        this == leader -> this
-        this.y == leader.y -> if (this.x < leader.x) this.right else this.left
-        this.x == leader.x -> if (this.y < leader.y) this.up else this.down
-        this.x < leader.x -> if (this.y < leader.y) this.up.right else this.down.right
-        else -> if (this.y < leader.y) this.up.left else this.down.left
-      }
-    } else this
+  private fun Position.follow(leader: Position): Position = if (this in leader.adjacentPositions) this else when {
+    y == leader.y -> if (x < leader.x) right else left
+    x == leader.x -> if (y < leader.y) up else down
+    x < leader.x -> if (y < leader.y) up.right else down.right
+    else -> if (y < leader.y) up.left else down.left
   }
 }
