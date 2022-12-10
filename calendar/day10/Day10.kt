1065edafa4497dcc9130ac9be9e00359163d9601
Raman Gupta <rocketraman@gmail.com>
2022-12-10 00:58:46 -0500

    Day 10 minor cleanup

--- calendar/day10/Day10.kt
+++ calendar/day10/Day10.kt
@@ -5,18 +5,7 @@ import Lines
 
 class Day10 : Day() {
   override fun part1(input: Lines): Any {
-    val registerX = mutableListOf(1)
-
-    input.forEach { instruction ->
-      if (instruction == "noop") {
-        registerX.add(registerX.last())
-      }
-      else if (instruction.startsWith("addx")) {
-        val delta = instruction.substring(5).toInt()
-        registerX.add(registerX.last())
-        registerX.add(registerX.last() + delta)
-      }
-    }
+    val registerX = buildRegisterOverAllCycles(input)
 
     return listOf(20, 60, 100, 140, 180, 220).sumOf {
       registerX[it - 1] * it
@@ -24,18 +13,7 @@ class Day10 : Day() {
   }
 
   override fun part2(input: Lines): Any {
-    val registerX = mutableListOf(1)
-
-    input.forEach { instruction ->
-      if (instruction == "noop") {
-        registerX.add(registerX.last())
-      }
-      else if (instruction.startsWith("addx")) {
-        val delta = instruction.substring(5).toInt()
-        registerX.add(registerX.last())
-        registerX.add(registerX.last() + delta)
-      }
-    }
+    val registerX = buildRegisterOverAllCycles(input)
 
     val crtPixels = List(40 * 6) { i ->
       val pixelIndex = i.mod(40)
@@ -47,5 +25,20 @@ class Day10 : Day() {
       .joinToString("\n") { it.joinToString("") }
 
     return "\n" + crt
+  }
+
+  private fun buildRegisterOverAllCycles(input: Lines): MutableList<Int> {
+    val registerX = mutableListOf(1)
+
+    input.forEach { instruction ->
+      if (instruction == "noop") {
+        registerX.add(registerX.last())
+      } else if (instruction.startsWith("addx")) {
+        val delta = instruction.substring(5).toInt()
+        registerX.add(registerX.last())
+        registerX.add(registerX.last() + delta)
+      }
+    }
+    return registerX
   }
 }
