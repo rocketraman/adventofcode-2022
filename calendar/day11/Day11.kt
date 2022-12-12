b0b22d3949bcc7ed8a34a26437ff4770cbb19e13
Raman Gupta <rocketraman@gmail.com>
2022-12-12 02:48:01 -0500

    Day 11 and 12

--- calendar/day11/Day11.kt
+++ calendar/day11/Day11.kt
@@ -4,11 +4,105 @@ import Day
 import Lines
 
 class Day11 : Day() {
+  data class Monkey(
+    val items: MutableList<Long>,
+    val operation: (Long) -> Long,
+    val test: MonkeyTest,
+    var inspections: Long = 0,
+  )
+
+  data class MonkeyTest(
+    val testDivisibleBy: Long,
+    val nextTrue: Int,
+    val nextFalse: Int,
+  )
+
+  val startingItemRegex = """  Starting items: (.*)""".toRegex()
+  val operationRegex = """  Operation: new = old (.) (.*)""".toRegex()
+  val testRegex = """  Test: divisible by (.*)""".toRegex()
+  val throwRegex = """    If (?:true|false): throw to monkey (.*)""".toRegex()
+
   override fun part1(input: Lines): Any {
-    TODO("Not yet solved")
+    return solve(input, 20) { _ -> { it / 3 } }
   }
 
   override fun part2(input: Lines): Any {
-    TODO("Not yet solved")
+    return solve(input, 10_000) { monkeys ->
+      // had to check the subreddit for this -- I noticed all the divisors were primes but too rusty on math
+      //  for the implication to sink in :-(
+      val primesLcm = monkeys
+        .map { it.test.testDivisibleBy }
+        .reduce { acc, l -> acc * l }
+      return@solve { it.mod(primesLcm) }
+    }
+  }
+
+  private fun solve(input: Lines, rounds: Int, worryReducerOf: (List<Monkey>) -> (Long) -> Long): Long {
+    val items = input
+      .filter { startingItemRegex.matches(it) }
+      .mapNotNull {
+        startingItemRegex
+          .matchEntire(it)!!.groups[1]?.value
+          ?.split(", ")
+          ?.map { it.toLong() }
+      }
+
+    val operations = input
+      .filter { operationRegex.matches(it) }
+      .map {
+        val (operand, multiplier) = operationRegex
+          .matchEntire(it)!!.destructured
+
+        val multiplierFn = when (multiplier) {
+          "old" -> { old: Long -> old }
+          else -> { _ -> multiplier.toLong() }
+        }
+
+        when (operand) {
+          "*" -> { old: Long -> old * multiplierFn(old) }
+          "+" -> { old: Long -> old + multiplierFn(old) }
+          else -> error("Invalid operand $operand")
+        }
+      }
+
+    val tests = input
+      .filter { testRegex.matches(it) || it.startsWith("    If ") }
+      .chunked(3)
+      .map {
+        val divisibleBy = testRegex
+          .matchEntire(it[0])!!.groups[1]?.value
+          ?.toLong()
+
+        val (monkeyTrue) = throwRegex.matchEntire(it[1])!!.destructured
+        val (monkeyFalse) = throwRegex.matchEntire(it[2])!!.destructured
+
+        MonkeyTest(divisibleBy!!, monkeyTrue.toInt(), monkeyFalse.toInt())
+      }
+
+    val monkeys = items.indices.map {
+      Monkey(
+        items = items[it].toMutableList(),
+        operation = operations[it],
+        test = tests[it],
+      )
+    }
+
+    val worryReducer = worryReducerOf(monkeys)
+
+    (1..rounds).forEach { _ ->
+      monkeys.forEach { monkey ->
+        val listIterator = monkey.items.listIterator()
+        for (item in listIterator) {
+          monkey.inspections++
+          listIterator.remove()
+          val worry = worryReducer(monkey.operation(item))
+          val testResult = worry.mod(monkey.test.testDivisibleBy) == 0L
+          monkeys[if (testResult) monkey.test.nextTrue else monkey.test.nextFalse].items.add(worry)
+        }
+      }
+    }
+
+    val twoActive = monkeys.map { it.inspections }.sortedByDescending { it }.take(2)
+    return twoActive[0] * twoActive[1]
   }
 }
--- calendar/day12/Day12.kt
+++ calendar/day12/Day12.kt
@@ -4,11 +4,169 @@ import Day
 import Lines
 
 class Day12 : Day() {
+  data class Position(val x: Int, val y: Int)
+
   override fun part1(input: Lines): Any {
-    TODO("Not yet solved")
+    val graph = input.map { it.toCharArray().asList() }
+
+    fun positionOf(c: Char): Position {
+      val y = graph.indexOfFirst { it.contains(c) }
+      val x = graph[y].indexOfFirst { it == c }
+      return Position(x, y)
+    }
+
+    val startPosition = positionOf('S')
+    val endPosition = positionOf('E')
+
+    fun Position.value() = when (val value = graph[y][x]) {
+      'S' -> 'a'
+      'E' -> 'z'
+      else -> value
+    }
+
+    fun Position.possibleMoves(previousPosition: Position?): List<Position> = listOf(
+      Position(x + 1, y),
+      Position(x - 1, y),
+      Position(x, y + 1),
+      Position(x, y - 1),
+    ).filter {
+      (previousPosition == null || it != previousPosition) &&
+        it != startPosition &&
+        it.x >= 0 &&
+        it.y >= 0 &&
+        it.y < graph.size &&
+        it.x < graph[0].size &&
+        it.value() - value() <= 1
+    }
+
+    val unvisitedNodes = buildSet {
+      for (y in graph.indices) {
+        for (x in graph[0].indices) {
+          add(Position(x, y))
+        }
+      }
+    }.toMutableSet()
+
+    var current = startPosition
+
+    val distances = buildMap {
+      for (y in graph.indices) {
+        for (x in graph[0].indices) {
+          val position = Position(x, y)
+          if (position == current) put(position, 0)
+          else put(position, Int.MAX_VALUE)
+        }
+      }
+    }.toMutableMap()
+
+    tailrec fun djikstraStep() {
+      current
+        .possibleMoves(null)
+        .filter { it in unvisitedNodes }
+        .forEach { position ->
+          if (distances.getValue(current) + 1 < distances.getValue(position)) {
+            distances += (position to distances.getValue(current) + 1)
+          }
+        }
+
+      // mark current node as visited
+      unvisitedNodes -= current
+
+      if (endPosition !in unvisitedNodes) {
+        return
+      } else {
+        val minUnvisited = unvisitedNodes.minBy { distances.getValue(it) }
+        current = minUnvisited
+        djikstraStep()
+      }
+    }
+
+    djikstraStep()
+
+    return distances.getValue(endPosition)
   }
 
   override fun part2(input: Lines): Any {
-    TODO("Not yet solved")
+    val graph = input.map { it.toCharArray().asList() }
+
+    fun positionOf(c: Char): Position {
+      val y = graph.indexOfFirst { it.contains(c) }
+      val x = graph[y].indexOfFirst { it == c }
+      return Position(x, y)
+    }
+
+    // any node with a
+    val startPosition = positionOf('a')
+    val endPosition = positionOf('E')
+
+    fun Position.value() = when (val value = graph[y][x]) {
+      'S' -> 'a'
+      'E' -> 'z'
+      else -> value
+    }
+
+    fun Position.possibleMoves(previousPosition: Position?): List<Position> = listOf(
+      Position(x + 1, y),
+      Position(x - 1, y),
+      Position(x, y + 1),
+      Position(x, y - 1),
+    ).filter {
+      (previousPosition == null || it != previousPosition) &&
+        it.x >= 0 &&
+        it.y >= 0 &&
+        it.y < graph.size &&
+        it.x < graph[0].size &&
+        it.value() != 'a' &&
+        it.value() - value() <= 1
+    }
+
+    val unvisitedNodes = buildSet {
+      for (y in graph.indices) {
+        for (x in graph[0].indices) {
+          if (Position(x, y) != startPosition) {
+            add(Position(x, y))
+          }
+        }
+      }
+    }.toMutableSet()
+
+    var current = startPosition
+
+    val distances = buildMap {
+      for (y in graph.indices) {
+        for (x in graph[0].indices) {
+          val position = Position(x, y)
+          // this time we initialize all nodes with a with start distance 0, as we can start from any of them
+          if (position.value() == 'a') put(position, 0)
+          else put(position, Int.MAX_VALUE)
+        }
+      }
+    }.toMutableMap()
+
+    tailrec fun djikstraStep() {
+      current
+        .possibleMoves(null)
+        .filter { it in unvisitedNodes }
+        .forEach { position ->
+          if (distances.getValue(current) + 1 < distances.getValue(position)) {
+            distances += (position to distances.getValue(current) + 1)
+          }
+        }
+
+      // mark current node as visited
+      unvisitedNodes -= current
+
+      if (endPosition !in unvisitedNodes) {
+        return
+      } else {
+        val minUnvisited = unvisitedNodes.minBy { distances.getValue(it) }
+        current = minUnvisited
+        djikstraStep()
+      }
+    }
+
+    djikstraStep()
+
+    return distances.getValue(endPosition)
   }
 }
